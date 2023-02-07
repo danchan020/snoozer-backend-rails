d703f2e1c479f0d58c73ac3c5d513b8f9ff1403f
Daniel <danchan020@gmail.com>
2023-02-07 03:24:27 -0800

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :alarms
+    
+    validates_presence_of :email, :username
+    validates_uniqueness_of :email, :username
+    validates_length_of :username, :max_length => 15
 end
