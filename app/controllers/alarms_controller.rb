dcac19deff1ab236396b67f41851969ebdff2039
Daniel <danchan020@gmail.com>
2023-02-07 16:05:23 -0800

    all controllers

--- app/controllers/alarms_controller.rb
+++ app/controllers/alarms_controller.rb
@@ -1,2 +1,30 @@
 class AlarmsController < ApplicationController
+    def index
+        render json: Alarm.all.order(created_at: :desc)
+    end
+
+    def show
+        render json: Alarm.find(params[:id])
+    end
+
+    def create
+        render json: Alarm.create!(alarm_params), status: :created
+    end
+
+    def update
+        alarm = Alarm.find(params[:id])
+        alarm.update!(alarm_params)
+        render json: alarm
+    end
+
+    def destroy
+        Alarm.find(params[:id]).destroy
+        head :no_content
+    end
+
+    private
+
+    def alarm_params
+        params.permit(:user_id, :alarm_before, :alarm_after, :alarm_increment, :alarm_name, :is_disabled)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :alarms
+
+    attr_assessor :old_password
     
     validates_presence_of :email, :username
     validates_uniqueness_of :email, :username
